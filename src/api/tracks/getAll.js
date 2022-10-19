8a3faa4784aeec7274f2ef188d25bc8f47dd05c8
Tamajin <wild.jlq@gmail.com>
2022-10-19 13:56:25 +0200

    route getAll done

--- src/api/tracks/getAll.js
+++ src/api/tracks/getAll.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-console */
+const database = require('../../database');
+
 module.exports = (req, res) => {
-  // your code here !
+  database
+    .query('SELECT * FROM track')
+    .then(([track]) => {
+      res.json(track);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving track from database');
+    });
 };
