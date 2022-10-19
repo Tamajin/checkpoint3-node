f970b1d72dc0967cb6d706770f9a4fede4042fb3
Tamajin <wild.jlq@gmail.com>
2022-10-19 12:14:00 +0200

    add route getTracks

--- src/api/albums/getTracks.js
+++ src/api/albums/getTracks.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-console */
+const database = require('../../database');
+
 module.exports = (req, res) => {
-  // your code here !
+  database
+    .query('SELECT * FROM track INNER JOIN album ON album.id=track.id_album')
+    .then(([track]) => {
+      res.json(track);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving album from database');
+    });
 };
