4bb7446168db4f7c3c611951852f3499072bb9c2
Tamajin <wild.jlq@gmail.com>
2022-10-19 14:12:44 +0200

    bonus : upgrade getTracks with INNER JOIN

--- src/api/albums/getTracks.js
+++ src/api/albums/getTracks.js
@@ -2,8 +2,13 @@
 const database = require('../../database');
 
 module.exports = (req, res) => {
+  const id = parseInt(req.params.id);
+
   database
-    .query('SELECT * FROM track INNER JOIN album ON album.id=track.id_album')
+    .query(
+      'SELECT t.title AS track, t.youtube_url, a.title AS album_title FROM track t INNER JOIN album a ON a.id=t.id_album WHERE id_album=?',
+      [id]
+    )
     .then(([track]) => {
       res.json(track);
     })
