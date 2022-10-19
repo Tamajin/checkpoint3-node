4d1716b4aa0420f2d26677eb153aa701ea9e8b9f
Tamajin <wild.jlq@gmail.com>
2022-10-19 11:58:39 +0200

    route getAll created

--- src/api/albums/getAll.js
+++ src/api/albums/getAll.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-console */
+const database = require('../../database');
+
 module.exports = (req, res) => {
-  // your code here !
+  database
+    .query('SELECT * FROM album')
+    .then(([album]) => {
+      res.json(album);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving- album from database');
+    });
 };
