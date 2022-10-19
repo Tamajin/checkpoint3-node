af3bb3c972e4407f9938e607c1ff6fd64c16073c
Tamajin <wild.jlq@gmail.com>
2022-10-19 13:57:14 +0200

    route getOne done

--- src/api/tracks/getOne.js
+++ src/api/tracks/getOne.js
@@ -1,3 +1,19 @@
+/* eslint-disable no-console */
+const database = require('../../database');
+
 module.exports = (req, res) => {
-  // your code here !
+  const id = parseInt(req.params.id);
+  database
+    .query('SELECT * FROM track where id = ?', [id])
+    .then(([track]) => {
+      if (track[0] != null) {
+        res.json(track[0]);
+      } else {
+        res.status(404).send('No track found');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving track from database');
+    });
 };
