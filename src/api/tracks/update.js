2d781f310d459afc06a084ff90da1470651bfab9
Tamajin <wild.jlq@gmail.com>
2022-10-19 14:00:02 +0200

    route update track done

--- src/api/tracks/update.js
+++ src/api/tracks/update.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-console */
+const database = require('../../database');
+
 module.exports = (req, res) => {
-  // your code here !
+  const { title, youtube_url, id_album } = req.body;
+  const id = parseInt(req.params.id);
+
+  database
+    .query(
+      'UPDATE track SET title = ?, youtube_url = ?, id_album = ? WHERE id = ?',
+      [title, youtube_url, id_album, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error editing track');
+    });
 };
