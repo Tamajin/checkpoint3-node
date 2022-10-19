f2c809cc4731cab5b59748ca3512723c47761a67
Tamajin <wild.jlq@gmail.com>
2022-10-19 12:35:08 +0200

    route put album created

--- src/api/albums/update.js
+++ src/api/albums/update.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-console */
+const database = require('../../database');
+
 module.exports = (req, res) => {
-  // your code here !
+  const { title, genre, picture, artist } = req.body;
+  const id = parseInt(req.params.id);
+
+  database
+    .query(
+      'UPDATE album SET title = ?, genre = ?, picture = ?, artist = ? WHERE id = ?',
+      [title, genre, picture, artist, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Not found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error editing album');
+    });
 };
